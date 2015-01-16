094856034392902c097765350138b9997c1fbae4
shangjianhong <shangjianhong@users.noreply.github.com>
2015-01-16 14:54:43 +0800

    Update test.java

--- src/javaDemo/test.java
+++ src/javaDemo/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world123456789");
+		System.out.println("hello world12345678910");
 	}
 
 }
